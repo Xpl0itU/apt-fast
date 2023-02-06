9c424a82fe225b27028203336ce86c770a70babc
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-02-06 15:00:19 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+apt-get update
+
+if ! type aria2c >/dev/null 2>&1; then
+  apt-get install -y aria2
+fi
+
+wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
+chmod +x /usr/local/sbin/apt-fast
+wget https://raw.githubusercontent.com/V10lator/NUSspli/master/apt-fast/apt-fast.conf -O /etc/apt-fast.conf
